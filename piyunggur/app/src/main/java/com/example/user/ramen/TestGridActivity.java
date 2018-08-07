0add64bfe7392718387b605c065b413051b499f0
Piyunggur <31981784+piyunggur@users.noreply.github.com>
2018-08-07 17:40:06 +0700

    add img ramens

---
+++ piyunggur/app/src/main/java/com/example/user/ramen/TestGridActivity.java
@@ -0,0 +1,13 @@
+package com.example.user.ramen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestGridActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testgrid_layout);
+    }
+}
