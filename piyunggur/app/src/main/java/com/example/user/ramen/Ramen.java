42675a607ef24cd9ffd29e90300978386f931b84
Piyunggur <31981784+piyunggur@users.noreply.github.com>
2018-08-08 18:12:40 +0700

    add testgrid2

---
+++ piyunggur/app/src/main/java/com/example/user/ramen/Ramen.java
@@ -0,0 +1,40 @@
+package com.example.user.ramen;
+
+public class Ramen {
+    String name;
+    int price;
+    int image;
+
+    public Ramen() {
+    }
+
+    public Ramen(String name, int price, int image) {
+        this.name = name;
+        this.price = price;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
